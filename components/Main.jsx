3a80b4fec12bbefe0080713da84114cf2b7aa3d7
ryskeldi2237 <ryskeldi2237@gmail.com>
2021-11-09 11:44:08 +0600

    add gsap

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,7 +1,13 @@
+import React, {useRef, useEffect} from 'react'
+import { gsap } from "gsap";
 export default function Main() {
+    const boxRef = useRef();
+    useEffect(() => {
+        gsap.to(boxRef.current, { rotation: "+=360" });
+      }, []);
     return (
         <main className="main">
-            <div className="main__title">The essential line</div>
+            <div className="main__title" ref={boxRef} >The essential line</div>
         </main>
     )
 }
